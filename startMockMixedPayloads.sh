e483947733e1a0c2cc451753a121010d7e018f2b
ka2 <kalvarez2@gmail.com>
2020-11-03 13:42:02 -0500

    mixed payload

--- startMockMixedPayloads.sh
+++ startMockMixedPayloads.sh
@@ -1,1 +1,1 @@
-node mockServer.js -p 8083 -f /home/ec2-user/orc/mock-api-server/public/  -a 10000 -b 10000 &
+node mockServer.js -p 8083 -f /home/ec2-user/orc/mock-api-server/public/mixed/  -a 10000 -b 10000 &
